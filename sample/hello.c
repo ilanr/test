4749bfdc4331cf590134b034bd09d848e739aab1
ilan <ilan.ceg@gmail.com>
2013-05-22 02:06:51 -0700

    merged

--- sample/hello.c
+++ sample/hello.c
@@ -5,15 +5,12 @@ int main()
     printf(" learning git \n");
     printf (" doing changes in test branch ");
 }
-
-
-
-
-
-
-
-
 void add_function(int a, int b)
 {
     return a+b;
+}
+    
+void sub_func(int a, int b)
+{
+    return a-b;
 }
